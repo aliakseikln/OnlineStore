32cc72da2b68215da5bce4dd12379158fb0ac962
Bee <aliakseikln@gmail.com>
2023-02-18 20:42:25 +0300

    Added Hilt injection and Nav graph

--- app/src/main/java/com/example/myapplication/activities/LoginRegisterActivity.kt
+++ app/src/main/java/com/example/myapplication/activities/LoginRegisterActivity.kt
@@ -3,7 +3,9 @@ package com.example.myapplication.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/myapplication/fragments/loginRegister/RegisterFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/loginRegister/RegisterFragment.kt
@@ -1,7 +1,72 @@
 package com.example.myapplication.fragments.loginRegister
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.myapplication.R
+import com.example.myapplication.data.User
+import com.example.myapplication.databinding.FragmentRegisterBinding
+import com.example.myapplication.util.Resource
+import com.example.myapplication.viewmodel.RegisterViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
-class RegisterFragment : Fragment(R.layout.fragment_register) {
+private const val TAG = "RegisterFragment"
+
+@AndroidEntryPoint
+class RegisterFragment : Fragment() {
+
+    private lateinit var binding: FragmentRegisterBinding
+    private val viewModel by viewModels<RegisterViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentRegisterBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            buttonRegister.setOnClickListener {
+                val user = User(
+                    edFirstName.text.toString().trim(),
+                    edLastName.text.toString().trim(),
+                    edEmail.text.toString().trim()
+                )
+                val password = edPassword.text.toString()
+                viewModel.createAccountWithEmailAndPassword(user, password)
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.register.collect {
+                when (it) {
+                    is Resource.Loading -> {
+                        binding.buttonRegister.startAnimation()
+                    }
+                    is Resource.Success -> {
+                        Log.d("test", it.data.toString())
+                        binding.buttonRegister.revertAnimation()
+                    }
+                    is Resource.Error -> {
+                        Log.d(TAG, it.message.toString())
+                        binding.buttonRegister.revertAnimation()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+    }
+
+
 }
